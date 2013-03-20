10a1bab2d3d343dac835f88b32a49605dbcfebb3
morungos <stuart@morungos.com>
2013-03-20 19:06:14 -0400

    Deleted more old code - lift stuff this time

--- heliotrope-war/src/main/scala/org/obiba/heliotrope/menu/StudyInfo.scala
+++
@@ -1,8 +0,0 @@
-package org.obiba.heliotrope.menu
-
-import org.obiba.heliotrope.domain.Study
-
-abstract class StudyInfo 
-case object NoSuchStudy extends StudyInfo
-case object AllStudies extends StudyInfo
-case class FullStudyInfo(s: Study) extends StudyInfo
--- heliotrope-war/src/main/scala/org/obiba/heliotrope/menu/SubjectInfo.scala
+++
@@ -1,8 +0,0 @@
-package org.obiba.heliotrope.menu
-
-import org.obiba.heliotrope.domain.Subject
-
-abstract class SubjectInfo 
-case object NoSuchSubject extends SubjectInfo
-case object AllSubjects extends SubjectInfo
-case class FullSubjectInfo(s: Subject) extends SubjectInfo
